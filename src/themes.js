425967dfa646c0aeace24d523f26a335b0fe68d9
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-21 20:12:20 -0500

    Creating themes file with standard colors

---
+++ src/themes.js
@@ -0,0 +1,21 @@
+const lightTheme = {
+    primaryColor: '#e60023',
+    fontColor: '#000000',
+    background: {
+        accent1: '#eeeeee',
+        accent2: '#dddddd',
+        accent3: '#cccccc'
+    }
+};
+
+const darkTheme = {
+    primaryColor: '#e60023',
+    fontColor: '#ffffff',
+    background: {
+        accent1: '#111111',
+        accent2: '#222222',
+        accent3: '#333333'
+    }
+};
+
+export { lightTheme, darkTheme };
