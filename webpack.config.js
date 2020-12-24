c0361cd5be23a4345c0778d6ca1624de5b69bbb3
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-23 21:43:12 -0500

    Setting up developement mode and surpressing source map errors in dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'eval-source-map',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
