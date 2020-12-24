234ecfe6028d35d31969fff4e2a704b22a6b6f52
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-24 03:05:55 -0500

    Formatting files

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,42 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'eval-source-map',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: '/',
-        port: 8080
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(svg|jpg|png)$/,
-                use: ['file-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html')
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'public/static' }
-            ]
-        })
-    ]
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: '/',
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|jpg|png)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/static' },
+      ],
+    }),
+  ],
 };
