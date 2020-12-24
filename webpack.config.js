65222ed36bfea67b31a224590a606a8c5e4fa8c6
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-23 19:00:28 -0500

    Switching double quotes to single quotes for consistency

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "public" }
+                { from: 'public' }
             ]
         })
     ]
