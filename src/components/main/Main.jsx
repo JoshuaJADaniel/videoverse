9a4a9d55e61c0b1f631976e1f60925aad3409184
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-25 15:28:31 -0500

    Fixing background image bug (caused by z-index + uneeded elements)

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,19 +6,15 @@ import Header from '../header/Header';
 const Wrapper = styled.main`
     height: 10000px;
     margin-left: ${(props) => props.theme.sidebarWidth};
-    background: ${(props) => props.theme.background.level1};
-`;
-
-const Background = styled.img`
-    top: -20%;
-    z-index: -1;
-    position: absolute;
-    width: calc(100% - ${(props) => props.theme.sidebarWidth});
+    background-color: ${(props) => props.theme.background.level1};
+    background-image: url(${backgroundImg});
+    background-repeat: repeat-y;
+    background-size: contain;
+    background-position-y: -5%;
 `;
 
 const Main = () => (
   <Wrapper>
-    <Background src={backgroundImg} />
     <Header />
   </Wrapper>
 );
