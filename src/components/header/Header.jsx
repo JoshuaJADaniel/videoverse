a1f589133d3400724a0087b011ab838fbc6f7d46
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-25 14:52:13 -0500

    Adding some back and forward navigation buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import SearchBar from './SearchBar';
 import ModeToggle from './ModeToggle';
+import BackForward from './BackForward';
 
 const Wrapper = styled.header.attrs({
   className: 'd-flex pl-4 pr-5',
@@ -9,6 +10,7 @@ const Wrapper = styled.header.attrs({
 
 const Header = () => (
   <Wrapper>
+    <BackForward />
     <SearchBar />
     <ModeToggle />
   </Wrapper>
