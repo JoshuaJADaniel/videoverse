8688ab9e3cda7cd6ab265d5586b74567ac73db27
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-26 21:46:03 -0500

    Move simplified functions into utilities and source from there

--- src/data/sampleHero.js
+++ src/data/sampleHero.js
@@ -1,12 +1,5 @@
+import getRandomBackdrop from "../utils/getRandomBackdrop";
 
-const randomColor = () => `${Math.floor(Math.random() * 0xFFFFFF).toString(16)}`;
-
-const randomImg = (text) => `https://dummyimage.com/1280x720/${randomColor()}/fff&text=${text}`;
-
-const sampleHero = []
-
-for (let i = 0; i < 5; i++) {
-  sampleHero.push(randomImg(i + 1));
-}
+const sampleHero = [...Array(5)].map((_, idx) => getRandomBackdrop(idx + 1));
 
 export default sampleHero;
