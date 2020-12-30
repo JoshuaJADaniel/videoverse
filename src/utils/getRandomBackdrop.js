71a582450d9e7f85193c523b91c2aaefbf2ef099
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-30 15:42:51 -0500

    Make backdrop text optional

--- src/utils/getRandomBackdrop.js
+++ src/utils/getRandomBackdrop.js
@@ -1,6 +1,6 @@
 import getRandomColor from "./getRandomColor";
 
 const getRandomBackdrop = (text) =>
-  `https://dummyimage.com/1280x720/${getRandomColor()}/fff&text=${text}`;
+  `https://dummyimage.com/1280x720/${getRandomColor()}/fff&text=${text ?? ''}`;
 
 export default getRandomBackdrop;
