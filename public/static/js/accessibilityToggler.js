755fb9220e040f2fa55282757ad7c87ef7685863
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-29 20:39:02 -0500

    Add accessibility script to detect mouse usage versus keyboard usage

---
+++ public/static/js/accessibilityToggler.js
@@ -0,0 +1,7 @@
+document.body.addEventListener("mousedown", function () {
+  document.body.classList.add("using-mouse");
+});
+
+document.body.addEventListener("keydown", function () {
+  document.body.classList.remove("using-mouse");
+});
