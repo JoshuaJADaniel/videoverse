c4bca47148953eb92bad3b3e13a38d11292df316
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-30 16:31:55 -0500

    Update row to use poster component

--- src/components/main/Row.jsx
+++ src/components/main/Row.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import Poster from "./Poster";
 
 const StyledSection = styled.section.attrs({
-  className: 'm-5 p-4',
+  className: "mx-5 p-4",
 })``;
 
 const StyledHeading = styled.h2.attrs({
-  className: 'mb-4',
+  className: "mb-4",
 })`
   color: ${(props) => props.theme.fontColor};
-`
+`;
 
-const Row = (props) => {
+const Row = ({ title }) => {
   return (
     <StyledSection>
-      <StyledHeading>{props.title}</StyledHeading>
+      <StyledHeading>{title}</StyledHeading>
       <div className="swiper-container">
         <div className="swiper-wrapper">
-          <div className="swiper-slide">Slide 1</div>
-          <div className="swiper-slide">Slide 2</div>
-          <div className="swiper-slide">Slide 3</div>
-          <div className="swiper-slide">Slide 4</div>
-          <div className="swiper-slide">Slide 5</div>
-          <div className="swiper-slide">Slide 6</div>
-          <div className="swiper-slide">Slide 7</div>
-          <div className="swiper-slide">Slide 8</div>
-          <div className="swiper-slide">Slide 9</div>
-          <div className="swiper-slide">Slide 10</div>
-          <div className="swiper-slide">Slide 11</div>
-          <div className="swiper-slide">Slide 13</div>
-          <div className="swiper-slide">Slide 14</div>
-          <div className="swiper-slide">Slide 15</div>
-          <div className="swiper-slide">Slide 16</div>
-          <div className="swiper-slide">Slide 17</div>
-          <div className="swiper-slide">Slide 18</div>
-          <div className="swiper-slide">Slide 19</div>
-          <div className="swiper-slide">Slide 20</div>
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
+          <Poster />
         </div>
         <button
           type="button"
@@ -51,6 +51,10 @@ const Row = (props) => {
       </div>
     </StyledSection>
   );
+};
+
+Row.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Row;
