a9a234ea5ce8bdda57b3c8bff623cfffcc386c05
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-29 19:26:19 -0500

    Create skeleton for a single row of elements

---
+++ src/components/main/Row.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Row = () => {
+  return (
+    <section>
+      <div className="swiper-container">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">Slide 1</div>
+          <div className="swiper-slide">Slide 2</div>
+          <div className="swiper-slide">Slide 3</div>
+          <div className="swiper-slide">Slide 4</div>
+          <div className="swiper-slide">Slide 5</div>
+          <div className="swiper-slide">Slide 6</div>
+          <div className="swiper-slide">Slide 7</div>
+          <div className="swiper-slide">Slide 8</div>
+          <div className="swiper-slide">Slide 9</div>
+          <div className="swiper-slide">Slide 10</div>
+          <div className="swiper-slide">Slide 11</div>
+          <div className="swiper-slide">Slide 13</div>
+          <div className="swiper-slide">Slide 14</div>
+          <div className="swiper-slide">Slide 15</div>
+          <div className="swiper-slide">Slide 16</div>
+          <div className="swiper-slide">Slide 17</div>
+          <div className="swiper-slide">Slide 18</div>
+          <div className="swiper-slide">Slide 19</div>
+          <div className="swiper-slide">Slide 20</div>
+        </div>
+        <div className="swiper-button-prev" />
+        <div className="swiper-button-next" />
+        <div className="swiper-scrollbar" />
+      </div>
+    </section>
+  );
+};
+
+export default Row;
