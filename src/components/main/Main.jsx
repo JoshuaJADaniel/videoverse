a9c76ed07078d7cd1bf74c7b28d02c25adaa4b10
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-29 19:29:37 -0500

    Utilize new row component for testing

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,11 +3,12 @@ import styled from "styled-components";
 import backgroundImg from "../../img/background.svg";
 import Header from "../header/Header";
 import Hero from "./Hero";
+import Row from "./Row";
 
 const Wrapper = styled.main`
   height: 10000px;
   margin-left: ${(props) => props.theme.sidebarWidth};
-  
+
   background-size: contain;
   background-position-y: -5%;
   background-repeat: repeat-y;
@@ -19,6 +20,7 @@ const Main = () => (
   <Wrapper>
     <Header />
     <Hero />
+    <Row />
   </Wrapper>
 );
 
