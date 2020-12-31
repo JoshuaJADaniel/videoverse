0dbaad983df53fb5abadd59aa87e9f7a9875eef1
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 14:40:23 -0500

    Remove trailing slash

--- src/data/configs.js
+++ src/data/configs.js
@@ -1,5 +1,5 @@
 const API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-const API_URL = "https://api.themoviedb.org/3/";
-const API_IMG_URL = "https://image.tmdb.org/t/p/";
+const API_URL = "https://api.themoviedb.org/3";
+const API_IMG_URL = "https://image.tmdb.org/t/p";
 
 export { API_KEY, API_URL, API_IMG_URL };
