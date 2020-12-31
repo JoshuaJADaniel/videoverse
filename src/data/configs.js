6d61f23d053a1c3e73193d80eb135661e803965a
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 14:40:04 -0500

    Fix export syntax

--- src/data/configs.js
+++ src/data/configs.js
@@ -2,4 +2,4 @@ const API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 const API_URL = "https://api.themoviedb.org/3/";
 const API_IMG_URL = "https://image.tmdb.org/t/p/";
 
-export default { API_KEY, API_URL, API_IMG_URL };
+export { API_KEY, API_URL, API_IMG_URL };
