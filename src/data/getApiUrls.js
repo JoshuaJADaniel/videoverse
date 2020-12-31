273940eeb4ff94b6ccb9f3ff495eb97334579635
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 17:26:56 -0500

    Update function name to better represent return value

--- src/data/getApiUrls.js
+++ src/data/getApiUrls.js
@@ -2,10 +2,10 @@ import { API_KEY, API_IMG_URL } from "./configs";
 
 const _ = require("lodash");
 
-const getTrendingUrl = ({ mediaType }) =>
+const getTrendingPath = ({ mediaType }) =>
   `/trending/${mediaType}/week?api_key=${API_KEY}`;
 
 const getBackdropUrl = ({ backdropPath }) =>
   `${API_IMG_URL}/w1280/${_.trimStart(backdropPath, "\\")}`;
 
-export { getTrendingUrl, getBackdropUrl };
+export { getTrendingPath, getBackdropUrl };
