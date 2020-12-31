9abc1154c513a2b647e312f7345a0fcb619c22bb
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 16:16:40 -0500

    Add utility functions for accessing API endpoints

---
+++ src/data/getApiUrls.js
@@ -0,0 +1,9 @@
+import { API_KEY, API_IMG_URL } from "./configs";
+
+const _ = require('lodash');
+
+const getTrendingUrl = ({mediaType}) => `/trending/${mediaType}/week?api_key=${API_KEY}`;
+
+const getBackdropUrl = ({backdropPath}) => `${API_IMG_URL}/w1280/${_.trimStart(backdropPath, "\\")}`;
+
+export { getTrendingUrl, getBackdropUrl };
