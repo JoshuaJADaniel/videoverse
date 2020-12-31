499385454712ff7292ee6b602b3ec015cc4c1e51
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 17:27:42 -0500

    Frame out trending movies state object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Swiper, { Navigation, Scrollbar } from "swiper";
 import { ThemeProvider } from "styled-components";
+import axios from "axios";
 import { lightTheme, darkTheme } from "../themes/themes";
 import GlobalStyles from "../themes/GlobalStyles";
+import { getTrendingPath } from "../data/getApiUrls";
+import { API_URL } from "../data/configs";
 import Sidebar from "./sidebar/Sidebar";
 import Main from "./main/Main";
 import "swiper/swiper-bundle.css";
 
 const App = () => {
   const [theme] = useState("dark");
+  const [trendingMovies, setTrendingMovies] = useState({});
   const getTheme = () => (theme === "light" ? lightTheme : darkTheme);
 
   useEffect(() => {
+    const tmdb = axios.create({
+      baseURL: API_URL,
+    });
+
     Swiper.use([Navigation, Scrollbar]);
 
     /* eslint-disable no-new */
@@ -30,13 +38,17 @@ const App = () => {
         hide: true,
       },
     });
+
+    tmdb.get(getTrendingPath({ mediaType: "movie" })).then((res) => {
+      setTrendingMovies(res.data);
+    });
   }, []);
 
   return (
     <ThemeProvider theme={getTheme}>
       <GlobalStyles />
       <Sidebar />
-      <Main />
+      <Main trendingMovies={trendingMovies} />
     </ThemeProvider>
   );
 };
