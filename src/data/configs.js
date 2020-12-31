cdd61f6828e0b4a52afe20fe51b963c558a80fa8
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2020-12-31 14:01:37 -0500

    Add configs for application

---
+++ src/data/configs.js
@@ -0,0 +1,5 @@
+const API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const API_URL = "https://api.themoviedb.org/3/";
+const API_IMG_URL = "https://image.tmdb.org/t/p/";
+
+export default { API_KEY, API_URL, API_IMG_URL };
