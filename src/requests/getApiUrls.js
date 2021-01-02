bb463c165a716061a8495c5cd701d17e7bed7c58
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-02 12:28:22 -0500

    Update file to contain a backdrop URL function and selectively import from lodash

--- src/requests/getApiUrls.js
+++ src/requests/getApiUrls.js
@@ -1,11 +1,13 @@
-import { API_KEY } from "./configs";
-
-const _ = require("lodash");
+import { API_IMG_URL, API_KEY } from "../data/configs";
+import { trimStart } from "lodash";
 
 const getTrendingPath = ({ mediaType }) =>
   `/trending/${mediaType}/week?api_key=${API_KEY}`;
 
 const getMoviePath = ({ movieId }) =>
-  `/movie/${movieId}?api_key=${API_KEY}`;
+  `/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
 
-export { getTrendingPath, getMoviePath };
+const getBackdropUrl = ({ backdropPath }) =>
+  `${API_IMG_URL}/w1280/${trimStart(backdropPath, "/")}`;
+
+export { getTrendingPath, getMoviePath, getBackdropUrl };
