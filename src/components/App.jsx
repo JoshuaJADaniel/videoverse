f825728398a4ee0d4e274a007fae703965eb70c0
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 00:13:51 -0500

    Update <App /> to handle requests and serve pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Movie from "../pages/Movie";
+import TvShow from "../pages/TvShow";
+import BrowseMovies from "../pages/BrowseMovies";
+import BrowseTvShows from "../pages/BrowseTvShows";
+import PageNotFound from "../pages/PageNotFound";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} exact />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/movie/:id">
+          <Movie />
+        </Route>
+        <Route exact path="/tv-show/:id">
+          <TvShow />
+        </Route>
+        <Route exact path="/browse/movies">
+          <BrowseMovies />
+        </Route>
+        <Route exact path="/browse/tv-shows">
+          <BrowseTvShows />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
       </Switch>
     </Router>
   );
