111184ea2a4defa5b9ff583b8534fce5356ec983
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 08:57:30 -0500

    Update <Sidebar />, rename variables, and restructure file

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,13 +2,28 @@ import React, { useContext } from "react";
 import styled, { ThemeContext } from "styled-components";
 import Navigation from "./Navigation";
 
-const Sidenav = styled.aside`
-  top: 0;
-  bottom: 0;
-  z-index: 1;
-  position: fixed;
+const Sidebar = () => {
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <LogoWrapper src={themeContext.logo} />
+      </LogoContainer>
+      <Navigation />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.aside`
   height: 100%;
   width: ${(props) => props.theme.sidebarWidth};
+
+  top: 0;
+  bottom: 0;
+  position: fixed;
+
+  z-index: 1;
   background: ${(props) => props.theme.background.level2};
 `;
 
@@ -20,18 +35,5 @@ const LogoWrapper = styled.img`
   width: 100%;
   height: auto;
 `;
-
-const Sidebar = () => {
-  const themeContext = useContext(ThemeContext);
-
-  return (
-    <Sidenav>
-      <LogoContainer>
-        <LogoWrapper src={themeContext.logo} />
-      </LogoContainer>
-      <Navigation />
-    </Sidenav>
-  );
-};
 
 export default Sidebar;
