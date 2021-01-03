6feb3fe3d05483d0593e55dd71e93ae0a359fcaa
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 08:26:12 -0500

    Update file structure and imports

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+
 import SearchBar from "./SearchBar";
 import ModeToggle from "./ModeToggle";
-import BackForward from "./BackForward";
-
-const Wrapper = styled.header.attrs({
-  className: "d-flex pl-3 pr-4",
-})``;
+import NavigateHistory from "./NavigateHistory";
 
 const Header = () => (
   <Wrapper>
-    <BackForward />
+    <NavigateHistory />
     <SearchBar />
     <ModeToggle />
   </Wrapper>
 );
+
+const Wrapper = styled.header.attrs({
+  className: "d-flex pl-3 pr-4",
+})``;
 
 export default Header;
