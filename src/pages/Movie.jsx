713500a094e5ab7a9c39bdc5fdff4913b82ea7d7
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 15:21:15 -0500

    Update <Movie /> to use global styles, sidebar and new static hero component

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+
+import { ThemeProvider } from "styled-components";
+import lightTheme from "../themes/light";
+import darkTheme from "../themes/dark";
+
+import MainWrapper from "../components/main/MainWrapper";
+import StaticHero from "../components/main/StaticHero";
+import Sidebar from "../components/sidebar/Sidebar";
+import GlobalStyles from "../styles/GlobalStyles";
 
 const Movie = () => {
   const { id } = useParams();
+  const [theme] = useState("dark");
+  const getTheme = () => (theme === "light" ? lightTheme : darkTheme);
 
-  return <h1>Movie: You requested movie {id}</h1>;
+  return (
+    <ThemeProvider theme={getTheme}>
+      <GlobalStyles />
+      <Sidebar />
+      <MainWrapper>
+        <StaticHero
+          rating={7}
+          outOf={10}
+          movieTitle="Sample Movie Title"
+          movieYear={2020}
+          movieGenres="Fantasy | Comedy | Thriller"
+          movieRuntime="202 min"
+        />
+      </MainWrapper>
+    </ThemeProvider>
+  );
 };
 
 export default Movie;
