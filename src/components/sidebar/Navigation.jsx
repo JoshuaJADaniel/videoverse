7cf8ec029656b6147a1e6fb2e5bdd745823bbda1
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 08:52:12 -0500

    Update <Navigation /> to use a more selective import and restructure file

--- src/components/sidebar/Navigation.jsx
+++ src/components/sidebar/Navigation.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import menuItems from "../../data/menuItems";
 import SvgContainer from "../common/SvgContainer";
+import { kebabCase } from "lodash";
 
-const _ = require("lodash");
+const Navigation = () => (
+  <NavUl>
+    {menuItems.map((item) => (
+      <NavItem key={kebabCase(`nav-${item.title}`)}>
+        <NavLink href={item.url}>
+          <CustomSvgContainer>{item.path}</CustomSvgContainer>
+          {item.title}
+        </NavLink>
+      </NavItem>
+    ))}
+  </NavUl>
+);
 
-const SvgContainerCustom = styled(SvgContainer).attrs({
+const CustomSvgContainer = styled(SvgContainer).attrs({
   className: "mr-4",
 })``;
 
@@ -23,32 +35,21 @@ const NavLink = styled.a.attrs({
   className: "nav-link d-flex align-items-center",
 })`
   height: 45px;
-  font-size: 14px;
-  border-radius: 4px;
   padding: 0 16px !important;
+
+  border-radius: 4px;
   transition: all 0.2s linear;
-  color: ${(props) => props.theme.fontColorNeutral} !important;
+
+  font-size: 14px;
+  color: ${(props) => props.theme.fontColorNeutral};
 
   &:hover {
-    color: ${(props) => props.theme.fontColor} !important;
+    color: ${(props) => props.theme.fontColor};
 
-    ${SvgContainer} {
+    & svg {
       stroke-width: 2;
     }
   }
 `;
-
-const Navigation = () => (
-  <NavUl>
-    {menuItems.map((item) => (
-      <NavItem key={_.kebabCase(`nav-${item.title}`)}>
-        <NavLink href={item.url}>
-          <SvgContainerCustom>{item.path}</SvgContainerCustom>
-          {item.title}
-        </NavLink>
-      </NavItem>
-    ))}
-  </NavUl>
-);
 
 export default Navigation;
