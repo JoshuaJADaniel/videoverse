a18a1619d5fd107a462a9de954937b6a4c5f561f
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 08:44:32 -0500

    Create separate file for accessibility style for reusability

---
+++ src/styles/accessibilityOutline.js
@@ -0,0 +1,8 @@
+const accessibilityOutline = `
+  outline-width: 1px;
+  outline-style: auto;
+  outline-offset: 3px;
+  outline-color: -webkit-focus-ring-color;
+`;
+
+export default accessibilityOutline;
