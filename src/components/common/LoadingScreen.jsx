e6b45c96bc20b7045200b009fe7321637fa8ea6c
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-03 19:55:05 -0500

    Fix issue with trying to access undefined function and add styles

--- src/components/common/LoadingScreen.jsx
+++ src/components/common/LoadingScreen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 const LoadingScreen = () => (
   <Wrapper>
@@ -9,12 +10,19 @@ const LoadingScreen = () => (
 
 const Wrapper = styled.div.attrs({
   className: "d-flex justify-content-center align-items-center",
-})``;
+})`
+  height: 100vh;
+  background: ${(props) => props.theme.background.level3};
+`;
 
-const Loader = styled.div({
+const Loader = styled.div.attrs({
   className: "spinner-border",
   role: "status",
-})``;
+})`
+  width: 60px;
+  height: 60px;
+  color: ${(props) => props.theme.primaryColor};
+`;
 
 const SrSpan = styled.span.attrs({
   className: "sr-only",
