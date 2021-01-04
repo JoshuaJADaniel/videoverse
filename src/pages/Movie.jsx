c6d7a1cdc942881fb6d2af79f994d64a32b729f7
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-04 10:09:29 -0500

    Update <Movie /> with loading screen and TMDB API call

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,32 +1,79 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
 import lightTheme from "../themes/light";
 import darkTheme from "../themes/dark";
 
+import tmdb from "../requests/tmdb";
+import { getMovieDetailsPath } from "../requests/getTmdbEndpointPaths";
+import { getMovieBackdropUrl } from "../requests/getTmdbEndpointUrls";
+
+import LoadingScreen from "../components/common/LoadingScreen";
 import MainWrapper from "../components/main/MainWrapper";
 import StaticHero from "../components/main/StaticHero";
 import Sidebar from "../components/sidebar/Sidebar";
-import GlobalStyles from "../styles/GlobalStyles";
+import Header from "../components/header/Header";
 
 const Movie = () => {
   const { id } = useParams();
+  const history = useHistory();
   const [theme] = useState("dark");
+  const [loading, setLoading] = useState(true);
+  const [movieDetails, setMovieDetails] = useState({});
   const getTheme = () => (theme === "light" ? lightTheme : darkTheme);
+
+  useEffect(() => {
+    tmdb
+      .get(getMovieDetailsPath(id))
+      .then((res) => {
+        let updatedMovieDetails = {
+          backdropPath: res.data.backdrop_path,
+          title: res.data.title,
+          overview: res.data.overview,
+          year: res.data.release_date,
+          genres: res.data.genres.map((item) => item.name),
+          runtime: res.data.runtime,
+          rating: res.data.vote_average,
+          tagline: res.data.tagline,
+          outOf: 10,
+        };
+
+        setMovieDetails(updatedMovieDetails);
+        setTimeout(() => setLoading(false), 500);
+      })
+      .catch(() => {
+        setTimeout(() => history.push("/404"), 500);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <ThemeProvider theme={getTheme}>
+        <GlobalStyles />
+        <Sidebar />
+        <MainWrapper>
+          <LoadingScreen />
+        </MainWrapper>
+      </ThemeProvider>
+    );
+  }
 
   return (
     <ThemeProvider theme={getTheme}>
       <GlobalStyles />
       <Sidebar />
       <MainWrapper>
+        <Header />
         <StaticHero
-          rating={7}
-          outOf={10}
-          movieTitle="Sample Movie Title"
-          movieYear={2020}
-          movieGenres="Fantasy | Comedy | Thriller"
-          movieRuntime="202 min"
+          backgroundImage={getMovieBackdropUrl(movieDetails.backdropPath)}
+          rating={movieDetails.rating}
+          outOf={movieDetails.outOf}
+          movieTitle={movieDetails.title}
+          movieYear={movieDetails.year}
+          movieGenres={movieDetails.genres && movieDetails.genres.join(" | ")}
+          movieRuntime={`${movieDetails.runtime} mins`}
         />
       </MainWrapper>
     </ThemeProvider>
