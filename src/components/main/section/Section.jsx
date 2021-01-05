2ced8efc825831fcdcf98e3cd64bdafb1b1bcf25
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-04 19:19:21 -0500

    Create <Section /> for standard section styling

---
+++ src/components/main/section/Section.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Section = ({ title, subtitle, children }) => (
+  <Wrapper>
+    <Title>{title}</Title>
+    {subtitle && <Subtitle>{subtitle}</Subtitle>}
+    {children && <ChildrenWrapper>{children}</ChildrenWrapper>}
+  </Wrapper>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  children: PropTypes.string,
+};
+
+const Wrapper = styled.section.attrs({
+  className: "mx-5 px-4",
+})`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Title = styled.h3.attrs({
+  className: "m-0",
+})``;
+
+const Subtitle = styled.h6.attrs({
+  className: "mt-3 mb-0 h5",
+})``;
+
+const ChildrenWrapper = styled.div.attrs({
+  className: "mt-3",
+})``;
+
+export default Section;
