3ef8e72f2c3fc3ab53e965ed20ee70e22e202a86
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-04 19:44:07 -0500

    Update imports to absolute imports, add prop type validation, and update theme properties

--- src/components/main/MainWrapper.jsx
+++ src/components/main/MainWrapper.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
-import backgroundImage from "../../images/background.svg";
+import backgroundImage from "images/background.svg";
 
-const MainWrapper = (props) => <Wrapper>{props.children}</Wrapper>;
+const MainWrapper = ({ children }) => <Wrapper>{children}</Wrapper>;
+
+MainWrapper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
+};
 
 const Wrapper = styled.main`
   height: auto;
   min-height: 100vh;
   margin-left: ${(props) => props.theme.sidebarWidth};
 
-  background-size: contain;
+  background-size: 100%;
   background-position-y: -5%;
   background-repeat: repeat-y;
   background-image: url(${backgroundImage});
-  background-color: ${(props) => props.theme.background.level1};
+  background-color: ${(props) => props.theme.defaultBackground};
 `;
 
 export default MainWrapper;
