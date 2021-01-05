b7265579ef99c30b136fad8735c4ff06c764c9b2
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-04 21:01:56 -0500

    Create composed component for cast poster

---
+++ src/components/main/poster/CastPoster.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Poster from "components/main/poster/Poster";
+import getRandomPoster from "utils/getRandomPoster";
+
+const CastPoster = ({ name, character, imageUrl, linkToProfile }) => {
+  return (
+    <Poster
+      title={name || "Unknown"}
+      imageUrl={imageUrl || getRandomPoster()}
+      subtitle={character ? `\"${character}\"` : '"Unknown"'}
+      linkUrl={linkToProfile}
+      badge="Cast"
+    />
+  );
+};
+
+CastPoster.propTypes = {
+  name: PropTypes.string,
+  imageUrl: PropTypes.string,
+  character: PropTypes.string,
+  linkToProfile: PropTypes.string.isRequired,
+};
+
+export default CastPoster;
