754035dfbf971d276bfa73f7966a610f008c5819
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-05 10:43:33 -0500

    Update prop types and remove title component

--- src/components/main/Row.jsx
+++ src/components/main/Row.jsx
@@ -1,60 +1,31 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import Poster from "./Poster";
+import initializeSwiper from "utils/initializeSwiper";
 
-const StyledSection = styled.section.attrs({
-  className: "mx-5 p-4",
-})``;
+const Row = ({ children }) => {
+  useEffect(() => {
+    initializeSwiper();
+  }, []);
 
-const StyledHeading = styled.h2.attrs({
-  className: "mb-4",
-})`
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const Row = ({ title }) => {
   return (
-    <StyledSection>
-      <StyledHeading>{title}</StyledHeading>
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-          <Poster />
-        </div>
-        <button
-          type="button"
-          aria-label="Previous"
-          className="swiper-button-prev"
-        />
-        <button
-          type="button"
-          aria-label="Next"
-          className="swiper-button-next"
-        />
-        <div className="swiper-scrollbar" />
-      </div>
-    </StyledSection>
+    <div className="swiper-container">
+      <div className="swiper-wrapper">{children}</div>
+      <button
+        type="button"
+        aria-label="Previous"
+        className="swiper-button-prev"
+      />
+      <button type="button" aria-label="Next" className="swiper-button-next" />
+      <div className="swiper-scrollbar" />
+    </div>
   );
 };
 
 Row.propTypes = {
-  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
 };
 
 export default Row;
