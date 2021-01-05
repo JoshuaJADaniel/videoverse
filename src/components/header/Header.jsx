91605333afd75a71b8bbd1a3e5bb3316c3aafe5e
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-04 21:38:36 -0500

    Add prop types

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Searchbar from "components/header/Searchbar";
 import ToggleMode from "components/header/ToggleMode";
 import NavigateHistory from "components/header/NavigateHistory";
 
-const Header = () => (
+const Header = ({ mode, setMode }) => (
   <Wrapper>
     <NavigateHistory />
     <Searchbar />
-    <ToggleMode />
+    <ToggleMode mode={mode} setMode={setMode} />
   </Wrapper>
 );
+
+Header.propTypes = {
+  mode: PropTypes.bool.isRequired,
+  setMode: PropTypes.func.isRequired,
+};
 
 const Wrapper = styled.header.attrs({
   className: "d-flex pl-3 pr-4",
