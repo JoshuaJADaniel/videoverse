d136815c80136dfd80a207b5665b8af072263ab9
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-06 21:41:39 -0500

    Clear up code where number is subbed rather than boolean

--- src/utils/getProfilePlaceholder.js
+++ src/utils/getProfilePlaceholder.js
@@ -1,7 +1,18 @@
 import manPlaceholder from "images/profile-man.png";
 import womanPlaceholder from "images/profile-woman.png";
 
-const getProfilePlaceholder = (isWoman) =>
-  isWoman ? womanPlaceholder : manPlaceholder;
+const isWoman = 1;
+const isMan = 2;
+
+const getProfilePlaceholder = (gender) => {
+  switch (gender) {
+    case isWoman:
+      return womanPlaceholder;
+    case isMan:
+      return manPlaceholder;
+  }
+
+  return manPlaceholder;
+};
 
 export default getProfilePlaceholder;
