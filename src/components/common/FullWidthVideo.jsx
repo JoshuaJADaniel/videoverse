cc2cb406b679b536c7244217b7ce2fd9de547f78
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-07 08:47:05 -0500

    Update <FullWidthVideo /> to be responsive

--- src/components/common/FullWidthVideo.jsx
+++ src/components/common/FullWidthVideo.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const FullWidthVideo = ({ youtubeEmbedLink }) => (
-  <iframe
-    width="1280"
-    height="720"
-    src={youtubeEmbedLink}
-    frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-  />
+  <Wrapper>
+    <iframe
+      width="1280"
+      height="720"
+      frameBorder="0"
+      src={youtubeEmbedLink}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </Wrapper>
 );
 
 FullWidthVideo.propTypes = {
   youtubeEmbedLink: PropTypes.string.isRequired,
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 0;
+  position: relative;
+
+  /* Maintains aspect ratio of 16/9 */
+  padding-bottom: 56.25%;
+
+  & iframe {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+`;
 
 export default FullWidthVideo;
