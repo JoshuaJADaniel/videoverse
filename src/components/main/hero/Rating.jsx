5e24ccaacc58162e8b0fce1b8fdb8fb700d1fbb6
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-07 08:52:58 -0500

    Move file and update imports to absolute

--- src/components/main/hero/Rating.jsx
+++ src/components/main/hero/Rating.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import SvgContainer from "../common/SvgContainer";
+import SvgContainer from "components/common/SvgContainer";
 
 const Rating = ({ rating, outOf }) => {
   if (rating) {
