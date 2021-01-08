6f4c7814f4f497a03a50e53a810aa55ea924e288
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-08 10:11:19 -0500

    Fix bug where falsy values are rendered

--- src/utils/extractMovieDetails.js
+++ src/utils/extractMovieDetails.js
@@ -3,15 +3,13 @@ import { getMovieBackdropUrl } from "requests/getTmdbEndpointUrls";
 const extractMovieDetails = (movieObject) => {
   let movieDetails = {
     title: movieObject.title,
-    tagline: movieObject.tagline,
     overview: movieObject.overview,
 
     year: movieObject.release_date,
     rating: movieObject.vote_average,
-    runtime: `${movieObject.runtime} mins`,
+    runtime: movieObject.runtime && `${movieObject.runtime} mins`,
     genres: movieObject.genres.map((item) => item.name),
 
-    backdropPath: movieObject.backdrop_path,
     imageUrl: getMovieBackdropUrl(movieObject.backdrop_path),
   };
 
