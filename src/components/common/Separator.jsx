8f227412bb1bd1272d007074fe780d2418a88889
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-08 13:43:01 -0500

    Remove required field since <Separator /> and add option for horizontal line

--- src/components/common/Separator.jsx
+++ src/components/common/Separator.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Separator = ({ verticalSpace }) => <Wrapper padding={verticalSpace} />;
+const Separator = ({ verticalSpace, line }) => (
+  <Wrapper padding={verticalSpace}>{line && <HorizontalLine />}</Wrapper>
+);
 
 Separator.propTypes = {
-  verticalSpace: PropTypes.number.isRequired,
+  verticalSpace: PropTypes.number,
+  line: PropTypes.bool,
 };
 
 const Wrapper = styled.div`
   padding-bottom: ${(props) => props.padding}px;
+`;
+
+const HorizontalLine = styled.hr`
+  margin: 0;
+  padding: 0;
+  border-bottom: 2px solid rgba(200, 200, 200, 0.4);
 `;
 
 export default Separator;
