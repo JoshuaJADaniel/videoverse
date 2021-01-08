e2828894a078d4f0e0bf9a03b1338fb5945427c8
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-08 08:30:37 -0500

    Remove check since proptypes already requires values by default

--- src/components/main/RatingDisplay.jsx
+++ src/components/main/RatingDisplay.jsx
@@ -3,22 +3,16 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import SvgContainer from "components/common/SvgContainer";
 
-const RatingDisplay = ({ rating, outOf }) => {
-  if (rating) {
-    return (
-      <RatingWrapper>
-        <SvgContainer>
-          <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-        </SvgContainer>
-        <RatingText>
-          {rating} / {outOf}
-        </RatingText>
-      </RatingWrapper>
-    );
-  }
-
-  return null;
-};
+const RatingDisplay = ({ rating, outOf }) => (
+  <RatingWrapper>
+    <SvgContainer>
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+    </SvgContainer>
+    <RatingText>
+      {rating} / {outOf}
+    </RatingText>
+  </RatingWrapper>
+);
 
 RatingDisplay.propTypes = {
   rating: PropTypes.number.isRequired,
