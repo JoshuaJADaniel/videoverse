a095c72f5f062e3f6ff489a3d5c3028d550555cf
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-09 13:48:55 -0500

    Simplify detail extraction function, add separate image URLs for poster/backdrop, and add check to keys before access

--- src/utils/extractMediaDetails.js
+++ src/utils/extractMediaDetails.js
@@ -1,4 +1,7 @@
-import { getBackdropImageUrl } from "requests/getTmdbEndpointUrls";
+import {
+  getPosterImageUrl,
+  getBackdropImageUrl,
+} from "requests/getTmdbEndpointUrls";
 
 const getRuntime = (runtimes) => {
   if (runtimes.length) {
@@ -13,36 +16,35 @@ const getRuntime = (runtimes) => {
   return "";
 };
 
-const extractMediaDetails = (media) => {
-  let mediaDetails = {
-    // Common to movies and TV shows
-    id: media.id,
-    status: media.status,
-    tagline: media.tagline,
-    overview: media.overview,
-    rating: media.vote_average,
-    title: media.title || media.name,
-    releaseDate: media.release_date || media.first_air_date,
-    genres: media.genres.map((item) => item.name),
-    runtime:
-      (media.runtime && getRuntime([media.runtime])) ||
-      (media.episode_run_time && getRuntime(media.episode_run_time)),
-    imageUrl:
-      (media.backdrop_path && getBackdropImageUrl(media.backdrop_path)) || "",
+const extractMediaDetails = (media) => ({
+  // Common to movies and TV shows
+  id: media.id,
+  status: media.status,
+  tagline: media.tagline,
+  overview: media.overview,
+  mediaType: media.media_type,
+  rating: media.vote_average,
+  title: media.title || media.name,
+  releaseDate: media.release_date || media.first_air_date,
+  genres: media.genres && media.genres.map((item) => item.name),
+  runtime:
+    (media.runtime && getRuntime([media.runtime])) ||
+    (media.episode_run_time && getRuntime(media.episode_run_time)),
+  posterImageUrl:
+    (media.poster_path && getPosterImageUrl(media.poster_path)) || "",
+  backdropImageUrl:
+    (media.backdrop_path && getBackdropImageUrl(media.backdrop_path)) || "",
 
-    // Specific to movies
-    budget: media.budget,
-    revenue: media.revenue,
+  // Specific to movies
+  budget: media.budget,
+  revenue: media.revenue,
 
-    // Specific to TV shows
-    lastDate: media.last_air_date,
-    creators: media.created_by,
-    totalEpisodes: media.number_of_episodes,
-    totalSeasons: media.number_of_seasons,
-    seasons: media.seasons,
-  };
-
-  return mediaDetails;
-};
+  // Specific to TV shows
+  lastDate: media.last_air_date,
+  creators: media.created_by,
+  totalEpisodes: media.number_of_episodes,
+  totalSeasons: media.number_of_seasons,
+  seasons: media.seasons,
+});
 
 export default extractMediaDetails;
