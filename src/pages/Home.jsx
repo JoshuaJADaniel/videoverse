2f5ddf2af5ff9291857dfa695621bac796777542
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-22 15:05:16 -0500

    Update <Home /> page with sections and content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,80 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import initializeSwiper from "../utils/initializeSwiper";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../styles/GlobalStyles";
-import lightTheme from "../themes/light";
-import darkTheme from "../themes/dark";
+import GlobalStyles from "styles/GlobalStyles";
 
-import Sidebar from "../components/sidebar/Sidebar";
+import getMedia from "requests/getMedia";
+import getMovies from "requests/getMovies";
+import getTvShows from "requests/getTvShows";
+
+import Loading from "pages/Loading";
+import resolveLoading from "utils/resolveLoading";
+import { getTheme, getLocalTheme } from "utils/themeFunctionality";
+
+import Header from "components/header/Header";
+import Sidebar from "components/sidebar/Sidebar";
+import MainWrapper from "components/main/MainWrapper";
+import CarouselHero from "components/main/hero/CarouselHero";
+import MediaSection from "components/main/section/MediaSection";
+import Separator from "components/common/Separator";
 
 const Home = () => {
-  const [theme] = useState("dark");
-  const getTheme = () => (theme === "light" ? lightTheme : darkTheme);
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
+  const [dark, setDark] = useState(getLocalTheme());
+  const [trendingMedia, setTrendingMedia] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [popularTvShows, setPopularTvShows] = useState([]);
+  const [popularKidsMovies, setPopularKidsMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
 
   useEffect(() => {
-    initializeSwiper();
+    getMedia(
+      "/trending/all/week",
+      (data) => {
+        setTrendingMedia(data);
+        resolveLoading(setLoading);
+      },
+      [],
+      history
+    );
+
+    getTvShows("/tv/popular", setPopularTvShows);
+    getMovies("/movie/popular", setPopularMovies);
+    getMovies("/movie/top_rated", setTopRatedMovies);
+    getMovies("/discover/movie", setPopularKidsMovies, [
+      "certification_country=US",
+      "certification.lte=G",
+      "with_genres=16",
+    ]);
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <ThemeProvider theme={getTheme}>
+    <ThemeProvider theme={getTheme(dark)}>
       <GlobalStyles />
       <Sidebar />
+      <MainWrapper>
+        <Header mode={dark} setMode={setDark} />
+        <CarouselHero mediaData={trendingMedia} />
+        <Separator verticalSpace={75} />
+        <MediaSection mediaData={trendingMedia} title="Trending" />
+        <Separator verticalSpace={75} />
+        <MediaSection mediaData={popularMovies} title="Popular Movies" />
+        <Separator verticalSpace={75} />
+        <MediaSection mediaData={popularTvShows} title="Popular TV Shows" />
+        <Separator verticalSpace={75} />
+        <MediaSection
+          mediaData={popularKidsMovies}
+          title="Popular Kids Movies"
+        />
+        <Separator verticalSpace={75} />
+        <MediaSection mediaData={topRatedMovies} title="Top Rated Movies" />
+      </MainWrapper>
     </ThemeProvider>
   );
 };
