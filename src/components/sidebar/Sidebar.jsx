aa5ba7afa351916ebbc2c4363a2c570bc784b2ba
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-26 18:41:39 -0500

    Remove styled-components and replace with .scss

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,50 +3,18 @@ import styled from "styled-components";
 import Navigation from "components/sidebar/Navigation";
 import logoImage from "images/logo-image.svg";
 import logoText from "images/logo-text.svg";
+import "./Sidebar.scss";
 
 const Sidebar = () => (
-  <Wrapper>
-    <LogoContainer>
-      <LogoImageWrapper src={logoImage} />
-      <LogoTextWrapper src={logoText} />
-    </LogoContainer>
+  <aside id="sidebar">
+    <div id="logo">
+      <a href="/">
+        <img alt="VideoVerse Logo" src={logoImage} />
+        <img alt="VideoVerse Text" src={logoText} />
+      </a>
+    </div>
     <Navigation />
-  </Wrapper>
+  </aside>
 );
-
-const Wrapper = styled.aside`
-  height: 100%;
-  width: ${(props) => props.theme.sidebarWidth};
-
-  top: 0;
-  bottom: 0;
-  position: fixed;
-
-  z-index: 1;
-  background: ${(props) => props.theme.sidebarBackground};
-`;
-
-const LogoContainer = styled.div.attrs({
-  className: "d-flex align-items-center",
-})`
-  padding: 22px;
-`;
-
-const LogoImageWrapper = styled.img`
-  width: 28px;
-  height: 28px;
-
-  flex-grow: 0;
-  flex-shrink: 0;
-
-  margin-right: 14px;
-`;
-
-const LogoTextWrapper = styled.img`
-  height: 20px;
-  width: auto;
-
-  filter: ${(props) => props.theme.logoFilter};
-`;
 
 export default Sidebar;
