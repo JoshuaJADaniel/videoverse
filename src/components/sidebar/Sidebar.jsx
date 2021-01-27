519b02eae9d51b949ecf1a631b20f360a05de39b
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-26 19:02:12 -0500

    Merge navigation into <Sidebar /> component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import styled from "styled-components";
-import Navigation from "components/sidebar/Navigation";
-import logoImage from "images/logo-image.svg";
+import { kebabCase } from "lodash";
+
 import logoText from "images/logo-text.svg";
+import logoImage from "images/logo-image.svg";
+import SvgContainer from "components/common/SvgContainer";
+import menuItems from "data/menuItems";
 import "./Sidebar.scss";
 
 const Sidebar = () => (
@@ -13,7 +15,18 @@ const Sidebar = () => (
         <img alt="VideoVerse Text" src={logoText} />
       </a>
     </div>
-    <Navigation />
+    <nav id="navigation">
+      <ul>
+        {menuItems.map((item) => (
+          <li key={kebabCase(`nav-${item.title}`)}>
+            <a href={item.url}>
+              <SvgContainer>{item.path}</SvgContainer>
+              <p>{item.title}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   </aside>
 );
 
