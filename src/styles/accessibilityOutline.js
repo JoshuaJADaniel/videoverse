692b391083821c95e5dc1288484668f881fea89d
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-01-30 14:54:18 -0500

    Delete unused files

--- src/styles/accessibilityOutline.js
+++
@@ -1,8 +0,0 @@
-const accessibilityOutline = `
-  outline-width: 1px;
-  outline-style: auto;
-  outline-offset: 3px;
-  outline-color: -webkit-focus-ring-color;
-`;
-
-export default accessibilityOutline;
