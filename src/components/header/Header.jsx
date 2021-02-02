00fb6ba0834b2362f82000d21442b753b08fb5fd
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-01 23:59:24 -0500

    Update theme toggle button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,9 +11,9 @@ const Header = ({ mode, setMode }) => (
     <div className={styles.menuContainer}>
       <button id="sidebarToggle" onClick={toggleMenu}>
         <SvgContainer>
-          <line x1="1" y1="4" x2="21" y2="4" />
-          <line x1="1" y1="12" x2="21" y2="12" />
-          <line x1="1" y1="20" x2="21" y2="20" />
+          <line x1="2" y1="4" x2="22" y2="4" />
+          <line x1="2" y1="12" x2="22" y2="12" />
+          <line x1="2" y1="20" x2="22" y2="20" />
         </SvgContainer>
       </button>
     </div>
@@ -39,15 +39,31 @@ const Header = ({ mode, setMode }) => (
       <input type="text" placeholder="Search..." />
     </form>
     <div className={styles.toggleThemeContainer}>
-      <input
-        type="checkbox"
-        checked={mode}
-        onChange={() => {
+      <button
+        className={mode ? styles.buttonRotation : styles.undoButtonRotation}
+        onClick={() => {
           setMode(!mode);
           toggleTheme();
         }}
-      />
-      <span />
+      >
+        {mode ? (
+          <SvgContainer>
+            <circle cx="12" cy="12" r="5" />
+            <line x1="12" y1="1" x2="12" y2="3" />
+            <line x1="12" y1="21" x2="12" y2="23" />
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+            <line x1="1" y1="12" x2="3" y2="12" />
+            <line x1="21" y1="12" x2="23" y2="12" />
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+          </SvgContainer>
+        ) : (
+          <SvgContainer>
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+          </SvgContainer>
+        )}
+      </button>
     </div>
   </div>
 );
