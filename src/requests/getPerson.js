6e4bd0e1176ab0b7494cab756c3b279602f1d2b4
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-16 12:40:27 -0500

    Update getPerson to extract proper data

--- src/requests/getPerson.js
+++ src/requests/getPerson.js
@@ -1,12 +1,29 @@
 import tmdb from "requests/tmdb";
+import { isEmpty } from "lodash";
 import { TMDB_KEY } from "data/configs";
 import redirectTo404 from "utils/redirectTo404";
+import extractPersonDetails from "utils/extractPersonDetails";
 
 const getPerson = (path, callback, routerHistoryObject) => {
   tmdb
     .get(`${path}?api_key=${TMDB_KEY}`)
     .then(({ data }) => {
-      callback(data);
+      let combinedData = {};
+      const possibleProperties = ["results", "crew", "cast"];
+
+      possibleProperties.forEach((property) => {
+        if (data[property]) {
+          combinedData[property] = data[property].map((person) =>
+            extractPersonDetails(person)
+          );
+        }
+      });
+
+      if (isEmpty(combinedData)) {
+        combinedData = extractPersonDetails(data);
+      }
+
+      callback(combinedData);
     })
     .catch((err) => {
       console.log(err);
