88f01771e12baa82a991a3d55d40c4fd882ae88e
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-17 10:44:09 -0500

    Update spacing

--- src/components/Spacer/Spacer.jsx
+++ src/components/Spacer/Spacer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Spacer = ({ space }) => <div style={{ paddingBottom: `${space}px` }} />;
+import styles from "./Spacer.module.scss";
+
+const Spacer = ({ space }) => {
+  if (space) {
+    return <div style={{ paddingBottom: `${space}px` }} />;
+  }
+
+  return <div className={styles.defaultSpacer} />;
+};
 
 Spacer.propTypes = {
-  space: PropTypes.number.isRequired,
+  space: PropTypes.number,
 };
 
 export default Spacer;
