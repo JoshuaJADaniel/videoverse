bfb5ca457b0d571f474b2578ca52214854116bf5
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-19 11:18:05 -0500

    Utilize useParams and pass required props

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import BrowseGeneral from "templates/BrowseGeneral";
 import extractPersonDetails from "utils/extractPersonDetails";
 
-const People = () => (
-  <BrowseGeneral
-    title="People"
-    pageLink="people"
-    pageName="People"
-    requestLink="/person/popular"
-    extractDetails={(peopleData) =>
-      peopleData.map((person) => extractPersonDetails(person))
-    }
-  />
-);
+const People = () => {
+  const { page } = useParams();
+
+  return (
+    <BrowseGeneral
+      title="People"
+      pageLink="people"
+      pageName="People"
+      requestLink="/person/popular"
+      currentPage={parseInt(page) || 1}
+      extractDetails={(peopleData) =>
+        peopleData.map((person) => extractPersonDetails(person))
+      }
+    />
+  );
+};
 
 export default People;
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import BrowseGeneral from "templates/BrowseGeneral";
 import extractMediaDetails from "utils/extractMediaDetails";
 
-const Trending = () => (
-  <BrowseGeneral
-    media
-    title="Trending"
-    pageLink="trending"
-    pageName="Trending"
-    requestLink="/trending/all/week"
-    extractDetails={(mediaData) =>
-      mediaData.map((media) => extractMediaDetails(media))
-    }
-  />
-);
+const Trending = () => {
+  const { page } = useParams();
+
+  return (
+    <BrowseGeneral
+      media
+      title="Trending"
+      pageName="Trending"
+      pageLink="trending"
+      requestLink="/trending/all/week"
+      currentPage={parseInt(page) || 1}
+      extractDetails={(mediaData) =>
+        mediaData.map((media) => extractMediaDetails(media))
+      }
+    />
+  );
+};
 
 export default Trending;
