05a1e0962bc5e54f54e29c4df9c07fcdae3d1acd
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-19 11:16:47 -0500

    Create pages with sorting capabilities

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const BrowseMovies = () => <h1>Browse Movies</h1>;
+import BrowseGeneral from "templates/BrowseGeneral";
+import extractMediaDetails from "utils/extractMediaDetails";
 
-export default BrowseMovies;
+const Movies = () => {
+  const { page } = useParams();
+  const defaultSort = { value: "revenue.desc", label: "Sort: Revenue" };
+
+  const sortByOptions = [
+    { value: "revenue.desc", label: "Sort: Revenue" },
+    { value: "popularity.desc", label: "Sort: Popularity" },
+    { value: "vote_count.desc", label: "Sort: Vote Count" },
+    { value: "vote_average.desc", label: "Sort: Vote Average" },
+  ];
+
+  return (
+    <BrowseGeneral
+      media
+      title="Movies"
+      pageName="Movies"
+      pageLink="movies"
+      requestLink="/discover/movie"
+      defaultSort={defaultSort}
+      sortByOptions={sortByOptions}
+      currentPage={parseInt(page) || 1}
+      extractDetails={(mediaData) =>
+        mediaData.map((media) => ({
+          ...extractMediaDetails(media),
+          mediaType: "movie",
+        }))
+      }
+    />
+  );
+};
+
+export default Movies;
--- src/pages/TvShows.jsx
+++ src/pages/TvShows.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const BrowseTv = () => <h1>Browse Tv Shows</h1>;
+import BrowseGeneral from "templates/BrowseGeneral";
+import extractMediaDetails from "utils/extractMediaDetails";
 
-export default BrowseTv;
+const TvShows = () => {
+  const { page } = useParams();
+  const defaultSort = { value: "popularity.desc", label: "Sort: Popularity" };
+
+  const sortByOptions = [
+    { value: "popularity.desc", label: "Sort: Popularity" },
+    { value: "vote_count.desc", label: "Sort: Vote Count" },
+    { value: "vote_average.desc", label: "Sort: Vote Average" },
+  ];
+
+  return (
+    <BrowseGeneral
+      media
+      title="TV Shows"
+      pageName="TV Shows"
+      pageLink="tv-shows"
+      requestLink="/discover/tv"
+      defaultSort={defaultSort}
+      sortByOptions={sortByOptions}
+      currentPage={parseInt(page) || 1}
+      extractDetails={(mediaData) =>
+        mediaData.map((media) => ({
+          ...extractMediaDetails(media),
+          mediaType: "tv",
+        }))
+      }
+    />
+  );
+};
+
+export default TvShows;
