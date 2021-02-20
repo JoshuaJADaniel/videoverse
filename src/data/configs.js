5c13cc0bd6847d7655bcbad1571ea8672d4dad67
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-20 17:20:33 -0500

    Change TMDB_KEY message

--- src/data/configs.js
+++ src/data/configs.js
@@ -1,4 +1,4 @@
-const TMDB_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const TMDB_KEY = "ENTER YOUR API";
 const TMDB_URL = "https://api.themoviedb.org/3";
 const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p";
 
